6e796ecb324e8d824aab11c20cade170bc6d3044
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2023-11-24 23:19:32 +0530

    singyp page

---
+++ chatApp/client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import cyan from './Images/cyan.png'
+import black from './Images/black.png'
+
+function App() {
+  return (
+    <div className='body-content'>
+      <img src={black} alt="black" className='black' />
+    <img src={cyan} alt="cyan" className='cyan' />
+    <div className="main">
+      <div className='signup'>
+        
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
