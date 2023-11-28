a78b7f8a63516fb573fad26923dab4a7d990cd19
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2023-11-28 12:58:19 +0530

    navbar

--- chatApp/client/src/components/Home.js
+++ chatApp/client/src/components/Home.js
@@ -1,27 +1,43 @@
-import React from 'react'
-import { Link,useNavigate } from 'react-router-dom'
-import { useAuthDispatch } from '../Context/auth'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuthDispatch } from "../Context/auth";
+import "../Styles/Home.css";
+import black from '../Images/black.png'
+import cyan from '../Images/cyan.png'
 
 function Home() {
+  const user = localStorage.getItem("token");
+  const navigate = useNavigate();
+  const dispatch = useAuthDispatch();
 
-  const dispatch = useAuthDispatch()
-  const navigate = useNavigate()
-
-  function logout(){
-    dispatch({type:'LOGOUT'})
-    navigate('/login')
+  function logout() {
+    dispatch({ type: "LOGOUT" });
+    navigate("/login");
   }
 
   return (
-    <div className='navbar'>
-        <div className="logo">WeChat</div>
+    <div className="main">
+      <img src={black} alt="black" className="black" />
+      <img src={cyan} alt="cyan" className="cyan" />
+      <div className="chat">
+      <div className="navbar">
+        <div className="logo"><i className="fa-brands fa-connectdevelop fa-spin"></i><span> VConnect</span></div>
         <div className="links">
-          <Link to='/register'>Register</Link>
-          <Link to='/login'>Login</Link>
-          <button className='logout-btn' onClick={logout}>Logout</button>
+          {!user ? (
+            <>
+              <Link to="/register">Register</Link>
+              <Link to="/login">Login</Link>
+            </>
+          ) : (
+            <button className="logout-btn" onClick={logout}>
+              <i className="fa-solid fa-right-from-bracket"></i> Logout
+            </button>
+          )}
         </div>
+      </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
